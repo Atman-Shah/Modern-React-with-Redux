1bb68e67cc84ee9effb9e4269c4f6379827dae06
Atman Shah <atman1339@gmail.com>
2023-05-26 14:43:04 +0530

    13th Video - Shorthand JS Expressions

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -10,12 +10,14 @@ const root = ReactDOM.createRoot(el);
 
 // 4. Create a component
 function App() {
-  let message = "Bye there";
-  if (Math.random() > 0.5) {
-    message = "Hello there";
-  }
+  // This is one approch
+  // let date = new Date();
+  // let time = date.toLocaleTimeString();
 
-  return <h1>{message}</h1>;
+  // return <h1>{time}</h1>;
+
+  // OR you can write JS expressions directly in the curly beackets
+  return <h1>{new Date().toLocaleTimeString()}</h1>;
 }
 
 // 5. Show the component on the screen.
