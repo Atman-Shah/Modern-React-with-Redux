d4a9e16be54c86590787a38c0f1acc17c593bc13
Atman Shah <atman1339@gmail.com>
2023-05-27 16:30:14 +0530

    38th Video - A Big Pile of HTML!

--- pdas/src/ProfileCrad.js
+++ pdas/src/ProfileCrad.js
@@ -1,9 +1,18 @@
 function ProfileCard({ title, handle, image }) {
   return (
-    <div>
-      <img alt="Personal Digital Assistants(pdas) Logos" src={image} />
-      <div>Title is {title}</div>
-      <div>Handle is {handle}</div>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img alt="Personal Digital Assistants(pdas) Logos" src={image} />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{title}</p>
+          <p className="subtitle is-6">{handle}</p>
+        </div>
+      </div>
     </div>
   );
 }
