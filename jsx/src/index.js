08c01f84bab7525f79ab8fa7d38e2568a5f509ff
Atman Shah <atman1339@gmail.com>
2023-05-27 14:31:05 +0530

    21st Video - Extracting Components

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,6 +1,7 @@
-// 1. Import React and ReactDOM libraries
+// 1. Import React, ReactDOM libraries and App component
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
 // 2. Get a reference to the div with id root.
 const el = document.getElementById("root");
@@ -8,19 +9,5 @@ const el = document.getElementById("root");
 // 3. Tell react to take control of that element.
 const root = ReactDOM.createRoot(el);
 
-// 4. Create a component
-function App() {
-  // const inputType = "number";
-  // const minValue = 5;
-
-  // return <input type={inputType} min={minValue} />;
-
-  // This is equivalent to the commented version above
-  return <input type="number" min={5} style={{ border: "3px solid red" }} />;
-  // We can provide strings, numbers, arrays, objects and variables as a prop.
-  // Note: If we try to display an object it doesn't work,
-  // however we can provide object as a prop. like we are providing one in style.
-}
-
-// 5. Show the component on the screen.
+// 4. Show the component on the screen.
 root.render(<App />);
