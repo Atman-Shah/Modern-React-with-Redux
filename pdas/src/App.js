e5e5bdf06373e444b23605edeabd0b4daa9b84fb
Atman Shah <atman1339@gmail.com>
2023-05-27 15:14:44 +0530

    25th Video - Creating Core Components

---
+++ pdas/src/App.js
@@ -0,0 +1,15 @@
+import ProfileCard from "./ProfileCrad";
+
+function App() {
+  return (
+    <div>
+      <div>Personal Digital Assisants</div>
+
+      <ProfileCard />
+      <ProfileCard />
+      <ProfileCard />
+    </div>
+  );
+}
+
+export default App;
