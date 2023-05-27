2a16ed3e3996f2f032b07e2d6ce111a75a6db86f
Atman Shah <atman1339@gmail.com>
2023-05-27 15:58:05 +0530

    34th Video - Including Images

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,9 +1,19 @@
 import ProfileCard from "./ProfileCrad";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+
+console.log(AlexaImage);
+console.log(SiriImage);
 
 function App() {
   return (
     <div>
       <div>Personal Digital Assisants</div>
+
+      <img alt="Alexa" src={AlexaImage} />
+      <img alt="Cortana" src={CortanaImage} />
+      <img alt="Siri" src={SiriImage} />
 
       <ProfileCard title="Alexa" handle="@alexa99" />
       <ProfileCard title="Cortana" handle="@cortana32" />
