8432a17d4094fc1bc37de9196733783da17f4421
Atman Shah <atman1339@gmail.com>
2023-06-01 14:25:59 +0530

    71st Video - Handling Input Elements

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,14 +1,22 @@
+import { useState } from "react";
+
 function SearchBar({ onSubmit }) {
+  const [term, setTerm] = useState("");
+
   const handleClick = (event) => {
     event.preventDefault();
 
     onSubmit("cars");
   };
 
+  const handleChange = (event) => {
+    setTerm(event.target.value);
+  };
+
   return (
     <div>
       <form onSubmit={handleClick}>
-        <input />
+        <input value={term} onChange={handleChange} />
       </form>
     </div>
   );
