0b08a1d34759ea47d7e3f73e6ae7cd351a0af522
Atman-Shah <atman1339@gmail.com>
2023-06-06 15:55:00 +0530

    Using useAtom to display and increment count

--- books/src/components/BookList.js
+++ books/src/components/BookList.js
@@ -1,9 +1,9 @@
 import BookShow from "./BookShow";
-import { useContext } from "react";
-import BooksContext from "../context/books";
+import { useAtom } from "jotai";
+import { valueAtom } from "../context/state";
 
 function BookList({ books, onDelete, onEdit }) {
-  const value = useContext(BooksContext);
+  const [value, setValue] = useAtom(valueAtom);
 
   const renderedBooks = books.map((book) => {
     return (
@@ -14,6 +14,9 @@ function BookList({ books, onDelete, onEdit }) {
   return (
     <div className="book-list">
       {value}
+      <button onClick={()=>{
+        setValue(value + 1);
+      }}>Click</button>
       {renderedBooks}
     </div>
   );
--- books/src/index.js
+++ books/src/index.js
@@ -2,13 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import BooksContext from "./context/books";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
 root.render(
-  <BooksContext.Provider value={5}>
     <App />
-  </BooksContext.Provider>
 );
