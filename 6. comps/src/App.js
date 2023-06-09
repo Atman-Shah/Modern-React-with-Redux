1d565cec6f22ad819c59932489a28066ab3047ef
Atman-Shah <atman1339@gmail.com>
2023-06-09 17:14:53 +0530

    157-159th Videos -  PropTypes in Action

--- 6. comps/src/App.js
+++ 6. comps/src/App.js
@@ -3,19 +3,19 @@ import Button from "./Button";
 function App() {
     return <div>
         <div>
-            <Button>Click me!</Button>
+            <Button primary rounded>Click me!</Button>
         </div>
         <div>
-            <Button>Buy now!</Button>
+            <Button success outlined rounded>Buy now!</Button>
         </div>
         <div>
-            <Button>Hide Ads!</Button>
+            <Button warning >Hide Ads!</Button>
         </div>
         <div>
-            <Button>See Deal!</Button>
+            <Button secondary outlined>See Deal!</Button>
         </div>
         <div>
-            <Button>Something!</Button>
+            <Button danger rounded>Something!</Button>
         </div>
     </div>;
 }
--- 6. comps/src/Button.js
+++ 6. comps/src/Button.js
@@ -1,5 +1,34 @@
-function Button({ children }){
+import PropTypes from "prop-types";
+
+function Button({ 
+    children,
+    primary,
+    secondary,
+    danger,
+    warning,
+    success,
+    outlined,
+    rounded,
+ }){
     return <button>{children}</button>
 }
+
+Button.propTypes = {
+    checkVariationValue: ({ primary,
+        secondary,
+        danger,
+        warning,
+        success }) => {
+        const count = Number(!!primary)
+        + Number(!!secondary)
+        + Number(!!warning)
+        + Number(!!danger)
+        + Number(!!success)
+
+        if(count > 1){
+            return new Error('Only one of primary, secondary, warning, danger, success can be true.')
+        }
+    },
+};
 
 export default Button;
