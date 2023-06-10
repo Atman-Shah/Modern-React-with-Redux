ffc20456c261ac4981746df1bf46e38856d1bec4
Atman-Shah <atman1339@gmail.com>
2023-06-10 23:00:26 +0530

    163th Video - Review on Styling

--- 6. comps/src/Button.js
+++ 6. comps/src/Button.js
@@ -10,7 +10,7 @@ function Button({
     outlined,
     rounded,
  }){
-    return <button className="bg-blue-500">{children}</button>
+    return <button className="px-3 py-1.5 border border-blue-600 bg-blue-500 text-white">{children}</button>
 }
 
 Button.propTypes = {
