9e20e2ef79c73524d10d78c0c0121585ca1b0147
Atman-Shah <atman1339@gmail.com>
2023-06-10 23:46:26 +0530

    165th Video -  Building Some Variations

--- 6. comps/src/Button.js
+++ 6. comps/src/Button.js
@@ -10,7 +10,16 @@ function Button({
     outlined,
     rounded,
  }){
-    return <button className="px-3 py-1.5 border border-blue-600 bg-blue-500 text-white">{children}</button>
+
+    const classes = className('px-3 py-1.5 border', {
+        'border-blue-600 bg-blue-500 text-white': primary,
+        'border-gray-900 bg-gray-900 text-white': secondary,
+        'border-red-500 bg-red-500 text-white': danger,
+        'border-yellow-400 bg-yellow-400 text-white': warning,
+        'border-green-500 bg-green-500 text-white': success,
+    });
+
+    return <button className={classes}>{children}</button>
 }
 
 Button.propTypes = {
