5e9149a661ec8587020fd12361e24891c79230de
Atman-Shah <atman1339@gmail.com>
2023-06-12 20:10:10 +0530

    176th Video -  Reminder on Building Lists

--- 6. comps/src/App.js
+++ 6. comps/src/App.js
@@ -3,14 +3,17 @@ import Accordion from "./components/Accordion";
 function App() {
     const items = [
         {
+            id: 1,
             label: "Can I use React on my project?",
             content: "You can use React on any projct. You can use React on any projct You can use React on any projct"
         },
         {
+            id: 2,
             label: "Can I use JavaScript on my project?",
             content: "You can use React on any projct. You can use React on any projct You can use React on any projct"
         },
         {
+            id: 3,
             label: "Can I use JavaScript on my project?",
             content: "You can use React on any projct. You can use React on any projct You can use React on any projct"
         }
