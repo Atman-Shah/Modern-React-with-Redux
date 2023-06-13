b484ce12f2f26cdd778ce5f896eb449a80d49132
Atman-Shah <atman1339@gmail.com>
2023-06-13 22:28:00 +0530

    180th video -  Conditional Rendering

--- 6. comps/src/components/Accordion.js
+++ 6. comps/src/components/Accordion.js
@@ -9,7 +9,10 @@ function Accordion({ items }) {
         return (
             <div key={item.id}>
                 <div>{item.label}</div>
-                <div>{item.content}</div>
+                {/* || gives back the 1st value that is truthy.
+                && gives back the 1st faulsy value or last truthy value */}
+                {/* So in the below case it'll return content only when isExpanded is true */}
+                {isExpanded && <div>{item.content}</div>}
             </div>
         );
     });
