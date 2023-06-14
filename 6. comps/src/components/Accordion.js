b87452585d1901c457e96b3c8afe708dc51b54c4
Atman-Shah <atman1339@gmail.com>
2023-06-14 15:17:50 +0530

    181th Video - Inline Event Handlers

--- 6. comps/src/components/Accordion.js
+++ 6. comps/src/components/Accordion.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function Accordion({ items }) {
+function Accordion({ items }) { 
     const [expandedIndex, setExpandedIndex] = useState(0);
 
     const renderedItems = items.map((item, index) => {
@@ -8,10 +8,10 @@ function Accordion({ items }) {
 
         return (
             <div key={item.id}>
-                <div>{item.label}</div>
+                <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
                 {/* || gives back the 1st value that is truthy.
                 && gives back the 1st faulsy value or last truthy value */}
-                {/* So in the below case it'll return content only when isExpanded is true */}
+                {/* So in the below case it'll  eturn content only when isExpanded is true */}
                 {isExpanded && <div>{item.content}</div>}
             </div>
         );
