60d611f721e9e688eab33aebb7bdb333d29b239b
Atman-Shah <atman1339@gmail.com>
2023-06-14 15:48:11 +0530

    182th Video -  Variation on Event Handlers

--- 6. comps/src/components/Accordion.js
+++ 6. comps/src/components/Accordion.js
@@ -3,6 +3,9 @@ import { useState } from "react";
 function Accordion({ items }) { 
     const [expandedIndex, setExpandedIndex] = useState(0);
 
+    // With all this extra work, we can now define an event handler outside of 
+    // the mapping function, which is good because we're not going to clutter up 
+    // the mapping function and make it hard to read.
     const handleClick = (nextIndex) => {
         setExpandedIndex(nextIndex);
     };
@@ -12,7 +15,13 @@ function Accordion({ items }) {
 
         return (
             <div key={item.id}>
+
+                {/* when we are trying to call that event handler, we're going to 
+                pass  in some information that was present only inside of the 
+                mapping function, i.e. only inside the mapping function we 
+                can access this index variable. */}
                 <div onClick={() => handleClick(index)}>{item.label}</div>
+
                 {/* || gives back the 1st value that is truthy.
                 && gives back the 1st faulsy value or last truthy value */}
                 {/* So in the below case it'll  eturn content only when isExpanded is true */}
