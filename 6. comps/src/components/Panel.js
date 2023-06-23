efea8bd460018afa245d7e4ed6a6e9c8fd1f8117
Atman-Shah <atman1339@gmail.com>
2023-06-23 14:11:34 +0530

    minor change

--- 6. comps/src/components/Panel.js
+++ 6. comps/src/components/Panel.js
@@ -1,7 +1,7 @@
 import classNames from "classnames";
 
 function Panel({ children, className, ...rest }) {
-    const finalClassNames = className(
+    const finalClassNames = classNames(
         'border rounded p-3 shadow bg-white w-full',
         className
         )
