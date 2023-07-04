2e673685b9a3d8cb859b4c2e79de915fad2241ae
Atman-Shah <atman1339@gmail.com>
2023-07-04 15:25:55 +0530

    241th Video - One Small Bug (scrolling issue)

--- 6. comps/src/pages/ModalPage.js
+++ 6. comps/src/pages/ModalPage.js
@@ -30,6 +30,36 @@ function ModalPage() {
     return <div className="relative">
         <Button primary onClick={handleClick}>Open Modal</Button>
         {showModal && modal}
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel leo ut ipsum aliquam ultrices nec ut metus. Curabitur eget elementum nibh. Sed tempor, lorem hendrerit iaculis dictum, felis risus consequat metus, at porta mauris metus vel mi. Phasellus tristique porttitor massa ac ullamcorper. Donec lobortis hendrerit accumsan. Pellentesque eget diam vel tellus varius suscipit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse at tortor vitae tellus condimentum accumsan. Cras rhoncus ante et eros condimentum mollis. Donec sagittis ipsum et sagittis feugiat. Etiam eu est sapien.
+        </p>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel leo ut ipsum aliquam ultrices nec ut metus. Curabitur eget elementum nibh. Sed tempor, lorem hendrerit iaculis dictum, felis risus consequat metus, at porta mauris metus vel mi. Phasellus tristique porttitor massa ac ullamcorper. Donec lobortis hendrerit accumsan. Pellentesque eget diam vel tellus varius suscipit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse at tortor vitae tellus condimentum accumsan. Cras rhoncus ante et eros condimentum mollis. Donec sagittis ipsum et sagittis feugiat. Etiam eu est sapien.
+        </p>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel leo ut ipsum aliquam ultrices nec ut metus. Curabitur eget elementum nibh. Sed tempor, lorem hendrerit iaculis dictum, felis risus consequat metus, at porta mauris metus vel mi. Phasellus tristique porttitor massa ac ullamcorper. Donec lobortis hendrerit accumsan. Pellentesque eget diam vel tellus varius suscipit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse at tortor vitae tellus condimentum accumsan. Cras rhoncus ante et eros condimentum mollis. Donec sagittis ipsum et sagittis feugiat. Etiam eu est sapien.
+        </p>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel leo ut ipsum aliquam ultrices nec ut metus. Curabitur eget elementum nibh. Sed tempor, lorem hendrerit iaculis dictum, felis risus consequat metus, at porta mauris metus vel mi. Phasellus tristique porttitor massa ac ullamcorper. Donec lobortis hendrerit accumsan. Pellentesque eget diam vel tellus varius suscipit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse at tortor vitae tellus condimentum accumsan. Cras rhoncus ante et eros condimentum mollis. Donec sagittis ipsum et sagittis feugiat. Etiam eu est sapien.
+        </p>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel leo ut ipsum aliquam ultrices nec ut metus. Curabitur eget elementum nibh. Sed tempor, lorem hendrerit iaculis dictum, felis risus consequat metus, at porta mauris metus vel mi. Phasellus tristique porttitor massa ac ullamcorper. Donec lobortis hendrerit accumsan. Pellentesque eget diam vel tellus varius suscipit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse at tortor vitae tellus condimentum accumsan. Cras rhoncus ante et eros condimentum mollis. Donec sagittis ipsum et sagittis feugiat. Etiam eu est sapien.
+        </p>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel leo ut ipsum aliquam ultrices nec ut metus. Curabitur eget elementum nibh. Sed tempor, lorem hendrerit iaculis dictum, felis risus consequat metus, at porta mauris metus vel mi. Phasellus tristique porttitor massa ac ullamcorper. Donec lobortis hendrerit accumsan. Pellentesque eget diam vel tellus varius suscipit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse at tortor vitae tellus condimentum accumsan. Cras rhoncus ante et eros condimentum mollis. Donec sagittis ipsum et sagittis feugiat. Etiam eu est sapien.
+        </p>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel leo ut ipsum aliquam ultrices nec ut metus. Curabitur eget elementum nibh. Sed tempor, lorem hendrerit iaculis dictum, felis risus consequat metus, at porta mauris metus vel mi. Phasellus tristique porttitor massa ac ullamcorper. Donec lobortis hendrerit accumsan. Pellentesque eget diam vel tellus varius suscipit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse at tortor vitae tellus condimentum accumsan. Cras rhoncus ante et eros condimentum mollis. Donec sagittis ipsum et sagittis feugiat. Etiam eu est sapien.
+        </p>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel leo ut ipsum aliquam ultrices nec ut metus. Curabitur eget elementum nibh. Sed tempor, lorem hendrerit iaculis dictum, felis risus consequat metus, at porta mauris metus vel mi. Phasellus tristique porttitor massa ac ullamcorper. Donec lobortis hendrerit accumsan. Pellentesque eget diam vel tellus varius suscipit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse at tortor vitae tellus condimentum accumsan. Cras rhoncus ante et eros condimentum mollis. Donec sagittis ipsum et sagittis feugiat. Etiam eu est sapien.
+        </p>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel leo ut ipsum aliquam ultrices nec ut metus. Curabitur eget elementum nibh. Sed tempor, lorem hendrerit iaculis dictum, felis risus consequat metus, at porta mauris metus vel mi. Phasellus tristique porttitor massa ac ullamcorper. Donec lobortis hendrerit accumsan. Pellentesque eget diam vel tellus varius suscipit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse at tortor vitae tellus condimentum accumsan. Cras rhoncus ante et eros condimentum mollis. Donec sagittis ipsum et sagittis feugiat. Etiam eu est sapien.
+        </p>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel leo ut ipsum aliquam ultrices nec ut metus. Curabitur eget elementum nibh. Sed tempor, lorem hendrerit iaculis dictum, felis risus consequat metus, at porta mauris metus vel mi. Phasellus tristique porttitor massa ac ullamcorper. Donec lobortis hendrerit accumsan. Pellentesque eget diam vel tellus varius suscipit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse at tortor vitae tellus condimentum accumsan. Cras rhoncus ante et eros condimentum mollis. Donec sagittis ipsum et sagittis feugiat. Etiam eu est sapien.
+        </p>
     </div>;
 }
 
