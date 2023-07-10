f45dc1aa07141309ec6ad07562c5767b04cf4a35
Atman-Shah <atman1339@gmail.com>
2023-07-10 18:26:29 +0530

    272th Video - Determining Icon Set

--- 6. comps/src/components/SortableTable.js
+++ 6. comps/src/components/SortableTable.js
@@ -2,56 +2,76 @@ import Table from "./Table";
 import { useState } from "react";
 
 function SortableTable(props) {
-    const { config, data } = props;
-    const [sortOrder, setSortOrder] = useState(null);
-    const [sortBy, setSortBy] = useState(null);
+  const { config, data } = props;
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
 
-    const handleClick = (label) => {
-        if(sortOrder === null) {
-            setSortOrder("asc");
-            setSortBy(label);
-        } else if(sortOrder === "asc") {
-            setSortOrder("desc");
-            setSortBy(label);
-        } else if(sortOrder === "desc") {
-            setSortOrder(null);
-            setSortBy(null);
-        }
+  const handleClick = (label) => {
+    if (sortOrder === null) {
+      setSortOrder("asc");
+      setSortBy(label);
+    } else if (sortOrder === "asc") {
+      setSortOrder("desc");
+      setSortBy(label);
+    } else if (sortOrder === "desc") {
+      setSortOrder(null);
+      setSortBy(null);
+    }
+  };
+
+  const updatedConfig = config.map((column) => {
+    if (!column.sortValue) {
+      return column;
+    }
+
+    return {
+      ...column,
+      header: () => (
+        <th onClick={() => handleClick(column.label)}>
+            {getIcons(column.label, sortBy, sortOrder)}
+          {column.label}
+        </th>
+      ),
     };
+  });
 
-     const updatedConfig = config.map((column) => {
-        if(!column.sortValue) {
-            return column;
-        }
+  let sortedData = data;
+  if (sortOrder && sortBy) {
+    const { sortValue } = config.find((column) => column.label === sortBy);
+    sortedData = [...data].sort((a, b) => {
+      const valueA = sortValue(a);
+      const valueB = sortValue(b);
 
-        return {
-            ...column,
-            header: () => <th onClick={() => handleClick(column.label)}>{column.label} IS SORTABLE</th>
-        };
-     });
+      const reverseOrder = sortOrder === "deascsc" ? 1 : -1;
 
-     let sortedData = data;
-     if(sortOrder && sortBy) {
-        const { sortValue } = config.find((column) => column.label === sortBy);
-        sortedData = [...data].sort((a, b) => {
-            const valueA = sortValue(a);
-            const valueB = sortValue(b);
+      if (typeof valueA === "string") {
+        return reverseOrder * valueA.localeCompare(valueB);
+      } else {
+        return reverseOrder * (valueA - valueB);
+      }
+    });
+  }
 
-            const reverseOrder = sortOrder === "deascsc" ? 1 : -1;
+  return (
+    <div>
+      {sortOrder} - {sortBy}
+      <Table {...props} data={sortedData} config={updatedConfig} />
+    </div>
+  );
+}
 
-            if(typeof valueA === "string") {
-                return reverseOrder * valueA.localeCompare(valueB);
-            } else {
-                return reverseOrder * (valueA - valueB);
-            }
+function getIcons(label, sortBy, sortOrder) {
+    if(label !== sortBy) {
+        return "Show both icons";
+    }
 
-        });
-     }
-
-  return (<div>
-    {sortOrder} - {sortBy}
-    <Table {...props} data={sortedData} config={updatedConfig} />
-    </div>);
+    if(sortOrder === null) {
+        return "Show both icons";
+    } else if(sortOrder === "asc") {
+        return "Show up icon";
+    } else if(sortOrder === "desc") {
+        return "Show down icon";
+    }
 }
 
 export default SortableTable;
